0482ce545f00595ebc535bda02fbe84f3fba321a
jefynhu11 <jefyx1@hotmail.com>
2024-06-17 21:39:36 -0300

    Adicionei um metodo aguardo para javascript e coloquei os comentarios

--- src/test/java/com/jeferson/javascript/Javascript.java
+++ src/test/java/com/jeferson/javascript/Javascript.java
@@ -7,16 +7,28 @@ import org.openqa.selenium.WebElement;
 
 public class Javascript {
     WebDriver driver;
+
+    /** Clica para Javascript **/
     public void javascriptClick() {
         WebElement element = driver.findElement(By.id("fulano"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
     }
+
+    /** Movimento mouse para Javascript **/
     public void javascriptScroll() {
         WebElement element = driver.findElement(By.id("fulano"));
         ((JavascriptExecutor) driver).executeScript("scroll(0,300)");
     }
+
+    /** Procura elemento para Javascript **/
     public void javascriptFindElement() {
         WebElement element = driver.findElement(By.id("fulano"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    /** Aguardo tempo para Javascript **/
+    public void javascriptWaitElement() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeAsyncScript("window.setTimeout(arguments[arguments.length - 1], 1000);");
     }
 }
