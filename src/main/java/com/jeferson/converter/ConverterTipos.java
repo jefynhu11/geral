8f0a320133b3ff75169e241a70438c6b53a0efeb
jefynhu11 <jefyx1@hotmail.com>
2024-06-24 00:30:58 -0300

    Adicionei dois metodos converteres double para String e String para double.

--- src/main/java/com/jeferson/converter/ConverterTipos.java
+++ src/main/java/com/jeferson/converter/ConverterTipos.java
@@ -8,6 +8,8 @@ public class ConverterTipos {
 
         StringToInt();
         IntToString();
+        DoubleToString();
+        StringToDouble();
 
     }
 
@@ -27,5 +29,23 @@ public class ConverterTipos {
         int numeroVezes2 = numeroConvertido * 2;
 
         System.out.println("Resultado: " + numeroVezes2);
+    }
+
+    private static void DoubleToString() {
+        double numero = 999;
+
+        String numString1 = String.valueOf(numero);
+        String numString2 = Double.toString(numero);
+        System.out.println("double para String Tipo1: " + numString1);
+        System.out.println("double para String Tipo2: " + numString2);
+    }
+
+    private  static void StringToDouble() {
+        String numeroString = "321645";
+
+        double numeroDouble1 = Double.parseDouble(numeroString);
+        double numeroDouble2 = Double.valueOf(numeroString);
+        System.out.println("String para double Tipo 1: " + numeroDouble1);
+        System.out.println("String para double Tipo 2: " + numeroDouble2);
     }
 }
